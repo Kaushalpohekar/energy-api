25c37a55f4f814aa063d264baa4f039c944c6047
Sohan Sarurkar <sohansarurkar47@gmail.com>
2023-12-27 13:22:07 +0530

    48 renewed

--- SL02202348.js
+++ SL02202348.js
@@ -37,7 +37,7 @@ mqttClient.on('connect', () => {
   console.log('Connected to MQTT broker');
 
   // Updated MQTT topics
-  const topics = Array.from({ length: 16 }, (_, i) => `Sense/Live/SL02202348/${i + 1}`);
+  const topics = Array.from({ length: 16 }, (_, i) => `Energy/Sense/Live/SL02202348/${i + 1}`);
 
   topics.forEach((topic) => {
     mqttClient.subscribe(topic, (error) => {
@@ -54,10 +54,17 @@ mqttClient.on('message', (topic, message) => {
   try {
     const data = JSON.parse(message);
 
+    const getMaxIdQuery = 'SELECT MAX(id) FROM public.energy_database';
+
+    pgClient.query(getMaxIdQuery)
+      .then((result) => {
+      const maxId = result.rows[0].max || 0;
+      // Increment the id for the new entry
+      const newId = maxId + 1;
 
     // Insert data into PostgreSQL database
-    const insertQuery = `INSERT INTO ems.ems_live (
-      date_time, device_uid, voltage_1n, voltage_2n, voltage_3n, voltage_n, voltage_12, voltage_23, voltage_31, voltage_l, 
+    const insertQuery = `INSERT INTO public.energy_database (
+      id,date_time, device_uid, voltage_1n, voltage_2n, voltage_3n, voltage_n, voltage_12, voltage_23, voltage_31, voltage_l, 
       current_1, current_2, current_3, "current", kw_1, kw_2, kw_3, kvar_1, kvar_2, kvar_3, kva_1, kva_2, kva_3, pf_1, pf_2, pf_3, 
       pf, freq, kw, kvar, kva, imp_kwh, exp_kwh, kwh, imp_kvarh, exp_kvarh, kvarh, kvah, thd_v1n, thd_v2n, thd_v3n, thd_v12, 
       thd_v23, thd_v31, thd_i1, thd_i2, thd_i3, max_kw, min_kw, max_kvar, min_kvar, max_int_v1n, max_int_v2n, max_int_v3n, 
@@ -65,24 +72,25 @@ mqttClient.on('message', (topic, message) => {
     ) VALUES (
       $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, 
       $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, 
-      $48, $49, $50, $51, $52, $53, $54, $55, $56, $57, $58, $59, $60, $61, $62, $63, $64
+      $48, $49, $50, $51, $52, $53, $54, $55, $56, $57, $58, $59, $60, $61, $62, $63, $64, $65
     )`;
 
     const insertValues = [
+      newId,
       data.date_time,
       data.device_uid,
-      data.voltage_1n,
-      data.voltage_2n,
-      data.voltage_3n,
-      data.voltage_n,
-      data.voltage_12,
-      data.voltage_23,
-      data.voltage_31,
-      data.voltage_l,
-      data.current_1,
-      data.current_2,
-      data.current_3,
-      data.current,
+      data.V_1n,
+      data.V_2n,
+      data.V_3n,
+      data.V_n,
+      data.V_12,
+      data.V_23,
+      data.V_31,
+      data.V_l,
+      data.I_1,
+      data.I_2,
+      data.I_3,
+      data.I,
       data.kw_1,
       data.kw_2,
       data.kw_3,
@@ -134,6 +142,7 @@ mqttClient.on('message', (topic, message) => {
       data.on_h,
       data.ser_no,
     ];
+    
 
     pgClient.query(insertQuery, insertValues)
       .then(() => {
@@ -142,6 +151,7 @@ mqttClient.on('message', (topic, message) => {
       .catch((error) => {
         console.error('Error inserting data into PostgreSQL:', error);
       });
+    })
   } catch (error) {
     console.error('Error processing message:', error);
   }
